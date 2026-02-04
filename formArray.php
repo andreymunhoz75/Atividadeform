79cac92a47f10a6cd4f912b31146d4d05cca54d1
Andrey Munhoz <andreymunhoz75@gmail.com>
2026-02-03 21:45:33 -0300

    Atividade

---
+++ formArray.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">Formulário Exemplo</h1>
+        </div>
+    </div>
+    <div class="row">
+        <form action="respform.php" method="post">
+            <div class="mb-3">
+                <label for="inputNome" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="inputNome" name="nome">
+            </div>
+            <div class="mb-3">
+                <label for="InputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="InputEmail1" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="InputTelefone" class="form-label">Telefone</label>
+                <input type="telefone" class="form-control" id="Inputtelefone" name="telefone">
+            </div>
+            <select class="form-select" aria-label="Default select example" name="periodo">
+                <option selected>Período</option>
+                <option value="Ténico em Informática">Ténico em Informática</option>
+                <option value="Ténico em Computação Gráfica">Ténico em Computação Gráfica</option>
+                <option value="Ténico em Disign Gráfico">Ténico em Disign Gráfico</option>
+            </select>
+            <div class="mb-3 form-check">
+                <h3>Cursos de Interesse</h3>
+                <input type="checkbox" class="form-check-input" id="checkTI" name="cursos[]" value="Manhã">
+                <label class="form-check-label" for="checkTI">Manhã</label><br>
+                <input type="checkbox" class="form-check-input" id="checkTCG" name="cursos[]" value="Tarde">
+                <label class="form-check-label" for="checkTCG">Tarde</label><br>
+                <input type="checkbox" class="form-check-input" id="checkDesign" name="cursos[]" value="Noite">
+                <label class="form-check-label" for="checkDesign">Noite</label><br>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
+</div>
---
+++ respform.php
@@ -0,0 +1,30 @@
+<?php
+
+$nome = filter_input(INPUT_POST,"nome",FILTER_DEFAULT);
+$email = filter_input(INPUT_POST,"email",FILTER_DEFAULT);
+$telefone = filter_input(INPUT_POST,"telefone",FILTER_DEFAULT);
+$curso = filter_input(INPUT_POST,"curso",FILTER_DEFAULT);
+
+if (trim($nome) === "" || $nome === null ||
+    trim($email) === "" || $email === null ||
+    trim($telefone) === "" || $telefone === null ){
+    header("Location: formArray.php");
+    exit;
+}
+
+$nomeSeguro = htmlspecialchars($nome,ENT_QUOTES, "UTF-8");
+$emailSeguro = htmlspecialchars($email,ENT_QUOTES, "UTF-8");
+$telefoneSeguro = htmlspecialchars($telefone,ENT_QUOTES, "UTF-8");
+$cursoSeguro = htmlspecialchars($curso,ENT_QUOTES, "UTF-8");
+
+
+echo "<span><strong>Nome: </strong>" . $nomeSeguro . "</span><br>";
+echo "<span><strong>Email: </strong>" . $emailSeguro . "</span><br>";
+echo "<span><strong>Telefone(s): </strong>" . $telefoneSeguro . "</span><br>";
+
+
+if (isset($_POST["cursos"])) {
+    $cursos = implode (', ', array_filter($_POST["cursos"]));
+
+    echo "<span><strong>Curso(s): </strong>" . $cursos . "</span><br>";
+}
